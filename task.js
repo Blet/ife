b3743c0323045d79e0781cb2b765dd748fc5e91c
jiazhaozhao <412986558@qq.com>
2016-06-23 11:00:04 +0800

    修改目录

---
+++ task.js
@@ -0,0 +1,262 @@
+window.taskJson=[
+    {
+        "任务一": "零基础HTML编码",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=1"
+    },
+    {
+        "任务二": "零基础HTML及CSS编码（一）",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=2"
+    },
+    {
+        "任务三": "三栏式布局",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=3"
+    },
+    {
+        "任务四": "定位和居中问题",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=4"
+    },
+    {
+        "任务五": "零基础HTML及CSS编码（二）",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=5"
+    },
+    {
+        "任务六": "通过HTML及CSS模拟报纸排版",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=6"
+    },
+    {
+        "任务七": "实现常见的技术产品官网的页面架构及样式布局",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=7"
+    },
+    {
+        "任务八": "响应式网格（栅格化）布局",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=8"
+    },
+    {
+        "任务九": "使用HTML/CSS实现一个复杂页面",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=9"
+    },
+    {
+        "任务十": "Flexbox 布局练习",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=10"
+    },
+    {
+        "任务十一": "移动Web页面布局实践",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=11"
+    },
+    {
+        "任务十二": "学习CSS 3的新特性",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=12"
+    },
+    {
+        "任务十三": "零基础JavaScript编码（一）",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=13"
+    },
+    {
+        "任务十四": "零基础JavaScript编码（二）",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=14"
+    },
+    {
+        "任务十五": "零基础JavaScript编码（三）",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=15"
+    },
+    {
+        "任务十六": "零基础JavaScript编码（四）",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=16"
+    },
+    {
+        "任务十七": "零基础JavaScript编码（五）",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=17"
+    },
+    {
+        "任务十八": "基础JavaScript练习（一）",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=18"
+    },
+    {
+        "任务十九": "基础JavaScript练习（二）",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=19"
+    },
+    {
+        "任务二十": "基础JavaScript练习（三）",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=20"
+    },
+    {
+        "任务二十一": "基础JavaScript练习（四）",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=21"
+    },
+    {
+        "任务二十二": "JavaScript和树（一）",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=22"
+    },
+    {
+        "任务二十三": "JavaScript和树（二）",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=23"
+    },
+    {
+        "任务二十四": "JavaScript和树（三）",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=24"
+    },
+    {
+        "任务二十五": "JavaScript和树（四）",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=25"
+    },
+    {
+        "任务二十六": "行星与飞船（一）",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=26"
+    },
+    {
+        "任务二十七": "行星与飞船（二）",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=27"
+    },
+    {
+        "任务二十八": "行星与飞船（三）",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=28"
+    },
+    {
+        "任务二十九": "表单（一）单个表单项的检验",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=29"
+    },
+    {
+        "任务三十": "表单（二）多个表单项的动态校验",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=30"
+    },
+    {
+        "任务三十一": "表单（三）联动",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=31"
+    },
+    {
+        "任务三十二": "表单（四）实现表单自动生成工厂",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=32"
+    },
+    {
+        "任务三十三": "听指令的小方块（一）",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=33"
+    },
+    {
+        "任务三十四": "听指令的小方块（二）",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=34"
+    },
+    {
+        "任务三十五": "听指令的小方块（三）",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=35"
+    },
+    {
+        "任务三十六": "听指令的小方块（四）",
+        "难度": "困难",
+        "链接": "http://ife.baidu.com/task/detail?taskId=36"
+    },
+    {
+        "任务三十七": "UI组件之浮出层",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=37"
+    },
+    {
+        "任务三十八": "UI组件之排序表格",
+        "难度": "简单",
+        "链接": "http://ife.baidu.com/task/detail?taskId=38"
+    },
+    {
+        "任务三十九": "UI组件之冻结行列表格",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=39"
+    },
+    {
+        "任务四十": "UI组件之日历组件（一）",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=40"
+    },
+    {
+        "任务四十一": "UI组件之日历组件（二）",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=41"
+    },
+    {
+        "任务四十二": "UI组件之日历组件（三）",
+        "难度": "困难",
+        "链接": "http://ife.baidu.com/task/detail?taskId=42"
+    },
+    {
+        "任务四十三": "多功能相册之拼图布局",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=43"
+    },
+    {
+        "任务四十四": "多功能相册之瀑布布局",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=44"
+    },
+    {
+        "任务四十五": "多功能相册之木桶布局",
+        "难度": "困难",
+        "链接": "http://ife.baidu.com/task/detail?taskId=45"
+    },
+    {
+        "任务四十六": "王牌特工 准备工作（一）",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=46"
+    },
+    {
+        "任务四十七": "王牌特工 准备工作（二）",
+        "难度": "中等",
+        "链接": "http://ife.baidu.com/task/detail?taskId=47"
+    },
+    {
+        "任务四十八": "王牌特工 准备工作（三）",
+        "难度": "困难",
+        "链接": "http://ife.baidu.com/task/detail?taskId=48"
+    },
+    {
+        "任务四十九": "王牌特工 准备工作（四）",
+        "难度": "困难",
+        "链接": "http://ife.baidu.com/task/detail?taskId=49"
+    },
+    {
+        "任务五十": "微型调查问卷平台",
+        "难度": "困难",
+        "链接": "http://ife.baidu.com/task/detail?taskId=50"
+    },
+    {
+        "任务五十一": "多功能相册",
+        "难度": "困难",
+        "链接": "http://ife.baidu.com/task/detail?taskId=51"
+    },
+    {
+        "任务五十二": "王牌特工",
+        "难度": "困难",
+        "链接": "http://ife.baidu.com/task/detail?taskId=52"
+    }
+]
